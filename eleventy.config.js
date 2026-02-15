a089a107fa5e97165f5a6c67a999b6185cbf1a10
Reuben Dalke <reuben@dalke.io>
2026-02-15 01:50:34 -0500

    Resolve remaining Eleventy-migration issues

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,24 +1,36 @@
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+import markdownIt from "markdown-it"
 
 export default function(eleventyConfig) {
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true,
+    typographer: true,
+  };
   eleventyConfig.addPlugin(feedPlugin, {
     type: "json", // or "rss", "json"
-    outputPath: "/iwt/feed.json",
+    outputPath: "/iwt/posts.json",
     collection: {
-      name: "posts", // iterate over `collections.posts`
+      name: "post", // iterate over `collections.posts`
       limit: 10,     // 0 means no limit
     },
     metadata: {
       language: "en",
       title: "Infinite, Worthless Time",
-      subtitle: "Notes from a temporary retirement",
+      description: "Notes from a temporary retirement",
       base: "https://cedeless.com/",
       author: {
         name: "Reuben Dalke",
-        email: "", // Optional
       }
     }
   });
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/*.png");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/safari-pinned-tab.svg");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
+  eleventyConfig.setLibrary("md", markdownIt(options));
 };
 
 export const config = {
--- src/iwt/iwt.11tydata.js
+++
@@ -1,9 +0,0 @@
-const { DateTime } = require("luxon");
-
-module.exports = {
-  layout: "blog_post",
-  page_class: "blog",
-  tags: ["posts"],
-  permalink:
-    "iwt/{{ page.date | date: '%Y/%m/%d' }}/{{ title | slugify }}/index.html",
-};
